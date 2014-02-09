adf29d0bff6c3b887288353f6d8ac1d0ba49eb61
szizor <szizor@gmail.com>
2014-02-08 23:55:18 -0700

    ban users/subscribe

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,15 @@ Imjuve::Application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
-
+  match 'users/subscribe' => redirect('/500.html')
   resources :users
   resources :sessions
   resources :surveys do
 	get :export, :on => :collection
   end
+
+
   root to: 'home#index'
+
 
 end
