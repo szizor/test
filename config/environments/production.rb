6a9d8da4da7888d49d82c85b51f191ca0064b069
szizor <szizor@gmail.com>
2014-02-11 19:24:30 -0700

    host on mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Imjuve::Application.configure do
   :password             => 'notigoto1',
   :authentication       => 'plain',
   :enable_starttls_auto => true  }
-  config.action_mailer.default_url_options = { :host => '66.228.51.135' }
+  ActionMailer::Base.default_url_options[:host] = "66.228.51.135"
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
