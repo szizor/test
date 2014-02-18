a202ac0cacb9c5c1a18c7ca7e6a5053be73ae12c
szizor <szizor@gmail.com>
2014-02-17 21:56:01 -0700

    test assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,13 +9,13 @@ Imjuve::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -46,8 +46,8 @@ Imjuve::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( encuestas.js, survey.css )
-  config.assets.precompile += %w( *.css *.js )
+  config.assets.precompile += %w( application.js application.css encuestas.js survey.css )
+  #config.assets.precompile += %w( *.css *.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
