5ca92d43372ba599ef23694151aec460b31c739f
Alejandro Arzola <szizor@gmail.com>
2014-06-08 17:46:02 -0500

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
 root = "/home/deploy/apps/imjuve/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+stderr_path "#{root}/log/unicornstd_err.log"
+stdout_path "#{root}/log/unicornstd_out.log"
 
 listen "/tmp/unicorn.imjuve.sock"
 worker_processes 2
