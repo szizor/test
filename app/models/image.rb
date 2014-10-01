51c4a79273f195e432dbbca873582ddf6f69702f
Alejandro Arzola <szizor@gmail.com>
2014-10-01 16:07:07 -0500

    remove validate

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ActiveRecord::Base
   attr_accessible :name, :image, :visible, :polygon_id, :remote_image_url
   mount_uploader :image, ImageUploader
-  validates_presence_of :name, :image
+  validates_presence_of :image
 end
