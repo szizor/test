7c836004e0e2ac5a2c59b9f458c385185cfbf51d
Alejandro Arzola <szizor@gmail.com>
2014-10-01 14:21:31 -0500

    prevent image to be saved withot data

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,5 @@
 class Image < ActiveRecord::Base
   attr_accessible :name, :image, :visible, :polygon_id, :remote_image_url
   mount_uploader :image, ImageUploader
+  validates_presence_of :name, :image
 end
