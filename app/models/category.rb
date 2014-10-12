49d30f6f2e69b39e1a9a73f58a94655821ec9784
Alejandro Arzola <szizor@gmail.com>
2014-10-11 19:46:50 -0500

    marker filter

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ActiveRecord::Base
   attr_accessible :name, :parent_category_id
   belongs_to :parent_category
+  def name_with_count
+  	"#{self.name} (#{counts})"
+  end
+
+  def counts
+  	Problem.where(:category_id => self.id).length
+  end
+
 end
