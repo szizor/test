95c372e35bf8ccfa25788deddfa68a4d69e42b82
Alejandro Arzola <szizor@gmail.com>
2014-10-18 23:19:10 -0500

    fixes

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ActiveRecord::Base
   attr_accessible :name, :parent_category_id
   belongs_to :parent_category
   def name_with_count
-  	"#{self.name} (#{counts})"
+  	"#{self.name}"
   end
 
   def counts
