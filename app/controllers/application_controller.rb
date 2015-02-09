e70cad36b8072ad47cfebbd5b8f900ace76ac082
Alejandro Arzola <szizor@gmail.com>
2015-02-08 23:04:16 -0600

    seletores

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
+  before_filter :authenticate #if Rails.env == "production"
 
   private
 
@@ -14,6 +15,12 @@ class ApplicationController < ActionController::Base
 
   def authorize_admin
     redirect_to root_url, alert: "No estas administrador" unless current_user && current_user.is_admin
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "test" && password == "Redespierta2015"
+    end
   end
 
 end
